f785e44517a1c98f8df126a7f1ad71ab522f8d65
unknown <isabella.manrique@labinfo.is.escuelaing.edu.co>
2023-11-02 13:58:32 -0500

    popup en progreso

--- src/pages/publicationDetailsPage.jsx
+++ src/pages/publicationDetailsPage.jsx
@@ -1,5 +1,5 @@
 import Navigation from "../components/Navigation/Navigation";
-import { Container, Wrap, Heading, Text, WrapItem } from "@chakra-ui/react";
+import { Container, Wrap, Heading, Text, WrapItem, Button, Popover, PopoverTrigger, PopoverBody, Box, Portal, PopoverContent } from "@chakra-ui/react";
 import { getPropertyById } from "../services/PropertyServices";
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 export default function PublicationDetailsPage() {
     const { id } = useParams();
     const [property, setProperty] = useState();
+    const [requested, setRequested] = useState(false);
 
     useEffect(() => {
             getPropertyById(id).then((res) => setProperty(res));
@@ -28,6 +29,28 @@ export default function PublicationDetailsPage() {
                     <Text py='2'>{property.description}</Text>
                     <Text py='2'>Metros cuadrados: {property.squareMeters}</Text>
                     <Text py='2'>Ubicacion: {property.location}</Text>
+                    <Button onClick={() => setRequested(true)} bg={"brand.300"} color={"white"} _hover={{bg: "brand.600"}}>Hacer Peticion</Button>
+                    <Popover>
+                    <PopoverTrigger>
+                        <Button>Hacer Peticion</Button>
+                    </PopoverTrigger>
+                    <Portal>
+                        <PopoverContent>
+                        <PopoverBody>
+                            <Box>
+                            Hello. Nice to meet you! This is the body of the popover
+                            </Box>
+                            {requested && (<Button
+                            mt={4}
+                            colorScheme='blue'
+                            onClick={setRequested(false)}
+                            >
+                            Close
+                            </Button>)}
+                        </PopoverBody>
+                        </PopoverContent>
+                    </Portal>
+                    </Popover>
                 </WrapItem>
             </Wrap>
             </Container>
---
+++ src/services/PetitionServices.js
@@ -0,0 +1,11 @@
+const server = "http://localhost:8080"
+
+export async function createPetitionForUser(petitionBody){
+    const res = await fetch(`${server}/v1/petition`, 
+    {method : 'POST',
+    headers: {
+            'Content-Type': 'application/json',
+            },
+    body : petitionBody});
+    return await res.json();
+}
